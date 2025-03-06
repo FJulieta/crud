06cfc655325f82a6b9c1bfdb2e18aa5967066646
Julieta Fader <julieta.fader@mercadolibre.com>
2025-03-06 15:49:05 -0300

    addMongodata

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("✅ Conectado a MongoDB");
+        const uri = process.env.MONGO_URI;
+        if (!uri) {
+            throw new Error("❌ ERROR: La URI de MongoDB no está definida en el archivo .env");
+        }
+
+        await mongoose.connect(uri);
+        console.log("✅ Conectado a MongoDB Local");
     } catch (error) {
         console.error("❌ Error al conectar MongoDB:", error);
         process.exit(1);
